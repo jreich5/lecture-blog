244ca33a8aab51ea319bde2b886b247b6b7c0af5
Justin Reich <justinreich.dev@gmail.com>
2018-04-11 15:19:50 -0500

    Initial commit

---
+++ src/test/java/com/codeup/lectureblog/LectureBlogApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codeup.lectureblog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LectureBlogApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
