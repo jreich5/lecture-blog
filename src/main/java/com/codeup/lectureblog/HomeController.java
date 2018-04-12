a6c7e02545afe6fdc2bf29bec473fdc5995b6c08
Justin Reich <justinreich.dev@gmail.com>
2018-04-12 13:46:52 -0500

    Complete view lecture

--- src/main/java/com/codeup/lectureblog/HomeController.java
+++ src/main/java/com/codeup/lectureblog/HomeController.java
@@ -1,8 +1,13 @@
 package com.codeup.lectureblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -11,4 +16,37 @@ public class HomeController {
     public String returnLandingMessage() {
         return "This is the landing page!";
     }
+
+    // returning a view
+    @GetMapping("/home")
+    public String welcome() {
+        return "home";
+    }
+
+    // returning a view with context variables
+    @GetMapping("/hi/{name}")
+    public String sayHello(@PathVariable String name, Model model) {
+        model.addAttribute("name", name);
+        return "home";
+
+    }
+
+
+    // if and each tags
+    @GetMapping("/all/{willSayHi}")
+    public String allHello(@PathVariable boolean willSayHi, Model model) {
+
+        List<String> names = new ArrayList<>();
+
+        names.add("Fred");
+        names.add("John");
+        names.add("Lisa");
+
+        model.addAttribute("names", names);
+        model.addAttribute("willSayHi", willSayHi);
+
+        return "home";
+
+    }
+
 }
