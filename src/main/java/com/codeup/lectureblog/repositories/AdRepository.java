4e6d2815373f208a46d49c7658b00a7bf996e0b2
Justin Reich <justinreich.dev@gmail.com>
2018-04-16 16:57:40 -0500

    Add repository annotation

--- src/main/java/com/codeup/lectureblog/repositories/AdRepository.java
+++ src/main/java/com/codeup/lectureblog/repositories/AdRepository.java
@@ -3,7 +3,9 @@ package com.codeup.lectureblog.repositories;
 
 import com.codeup.lectureblog.models.Ad;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AdRepository extends CrudRepository<Ad, Long> {
 
 }
